67252583c84cbf06365c58959224084c612b84d7
Moiré <git@moire.cx>
2026-05-28 00:53:37 -0700

    6.2 post test

---
+++ lab6_2/server.py
@@ -0,0 +1,16 @@
+import flask
+
+app = flask.Flask("weather_server")
+
+@app.route("/", methods = ["POST"])
+def post_recv():
+	text = flask.request.get_data(as_text=True)
+	print(text)
+	return ""
+
+@app.route("/location", methods = ["GET"])
+def location():
+	return "Santa+Cruz"
+
+if __name__ == "__main__":
+	app.run(host = "0.0.0.0", port = 1234)
